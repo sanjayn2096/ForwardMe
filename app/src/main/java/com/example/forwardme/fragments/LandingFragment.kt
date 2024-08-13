ee49228a619b34457c27475d9b21433e87cb2f9c
Sanjay Nagaraj <ngsnj@amazon.com>
2024-08-13 13:07:09 -0700

    Adding Message filter fragment and editing Nav graph

--- app/src/main/java/com/example/forwardme/fragments/LandingFragment.kt
+++ app/src/main/java/com/example/forwardme/fragments/LandingFragment.kt
@@ -1,18 +1,22 @@
-package com.example.forwardme
+package com.example.forwardme.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.forwardme.Data.ForwardingViewModel
+import com.example.forwardme.R
 
 class LandingFragment : Fragment(R.layout.fragment_landing) {
 
     private lateinit var forwardingNumberTextView: TextView
     private lateinit var numberDisplayBox: TextView
     private lateinit var changeNumberButton: Button
+    private lateinit var changeMessageFilter: Button
 
     private val viewModel: ForwardingViewModel by activityViewModels()
 
@@ -22,16 +26,19 @@ class LandingFragment : Fragment(R.layout.fragment_landing) {
         forwardingNumberTextView = view.findViewById(R.id.forwardingNumberTextView)
         numberDisplayBox = view.findViewById(R.id.numberDisplayView)
         changeNumberButton = view.findViewById(R.id.changeNumberButton)
+        changeMessageFilter = view.findViewById(R.id.changeFilterButton)
 
         viewModel.forwardNumber.observe(viewLifecycleOwner) { savedNumber ->
             numberDisplayBox.text = savedNumber
         }
 
         changeNumberButton.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.container, SetupFragment())
-                .addToBackStack(null)
-                .commit()
+            Log.e("LandingFragment", "Change Number Being Clicked")
+            findNavController().navigate(R.id.action_landingFragment_to_setupFragment)
+        }
+
+        changeMessageFilter.setOnClickListener{
+            findNavController().navigate(R.id.action_landingFragment_to_messageFilterFragment)
         }
     }
 }
